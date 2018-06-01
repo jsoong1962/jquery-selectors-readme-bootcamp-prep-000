d618370c53094f1fc9e94bf05049b41a21ba9266
Johnny Soong <johnnysoong@hotmail.com>
2018-06-01 18:49:19 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,2 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p');
-}
-
-function lastImageSelector() {
-  $('#pics img:last');
-}
-
-function ninjaBabySelector() {
-  $('#pics #baby-ninja');
-}
-
-function divSelector() {
-  $('div');
-}
-
-function firstListItem() {
-  $('#pic-list ul li:first-child');
-}
