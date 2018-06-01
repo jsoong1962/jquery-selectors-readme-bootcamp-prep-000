51a51e1a5d39cc5862c7efd4bf86fae5437bcb13
Johnny Soong <johnnysoong@hotmail.com>
2018-06-01 18:49:27 +0000

    automatically backed up by learn

---
+++ js/selectors.js
@@ -0,0 +1,19 @@
+function paragraphSelector() {
+//   $('p');
+// }
+
+// function lastImageSelector() {
+//   $('#pics img:last');
+// }
+
+// function ninjaBabySelector() {
+//   $('#pics #baby-ninja');
+// }
+
+// function divSelector() {
+//   $('div');
+// }
+
+// function firstListItem() {
+//   $('#pic-list ul li:first-child');
+// }
